1fbd20e6ca7633b0a00888ae59437daa4d57c4a3
alex <iesekela@mail.ru>
2023-02-24 15:09:54 +0300

    adding index

--- python/base_client.py
+++ python/base_client.py
@@ -14,35 +14,96 @@ from couchbase.options import (ClusterOptions, ClusterTimeoutOptions,
 from couchbase.transcoder import RawBinaryTranscoder
 
 # Update this to your cluster
-private_data = read_configs()
-username = private_data.Username
-password = private_data.Password
+# private_data = read_configs()
+# username = private_data.Username
+# password = private_data.Password
 # bucket_name = "travel-sample"
-bucket_name = "object-data"
+# bucket_name = "object-data"
 # User Input ends here.
 
 # Connect options - authentication
-auth = PasswordAuthenticator(
-    username,
-    password,
-)
+# auth = PasswordAuthenticator(
+#     username,
+#     password,
+# )
+
+class couchbaseClient:
+    def __init__(self, server = 'couchbase://127.0.0.1', configs = read_configs()):
+        auth = PasswordAuthenticator(
+            configs.Username,
+            configs.Password,
+        )
+        self.cluster = Cluster(server, ClusterOptions(auth))
+        self.cluster.wait_until_ready(timedelta(seconds=5))
+        self.bucket = self.cluster.bucket("dating-data")
+        self.user_data_collection = self.bucket.scope("protobufs").collection("user_data")
+        self.user_data_transcoder = RawBinaryTranscoder()
+
+        self.geo_data_collection = self.bucket.scope("indexing_jsons").collection("geo_data")
+
+    def insert_user(self, user: user_pb2.TUser):
+        try:
+            self.__insert_user(user)
+            self.__insert_to_external(user)
+        except Exception as e:
+            print(e)
+            raise e
+
+    def __insert_user(self, user: user_pb2.TUser):
+        result = self.user_data_collection.upsert(
+            str(user.Key.Hash),
+            user.SerializeToString(),
+            UpsertOptions(transcoder=self.user_data_transcoder)
+        )
+        print(result)
+
+    def read_user(self, key: user_pb2.TUserKey):
+        try:
+            return self.__read_user(key)
+        except Exception as e:
+            print(e)
+            raise e
+
+    def __read_user(self, key: user_pb2.TUserKey):
+        result = self.user_data_collection.get(str(key.Hash), GetOptions(transcoder=self.user_data_transcoder))
+        user = user_pb2.TUser()
+        user.ParseFromString(result.content_as[bytes])
+        return user
+
+    def __insert_to_external(self, user: user_pb2.TUser):
+        def make_geo_format(user: user_pb2.TUser):
+            return {
+                "geo": {
+                    "lat" : user.LastGeo.Latitude,
+                    "lon" : user.LastGeo.Longitude
+                }
+            }
+        try:
+            result = self.geo_data_collection.upsert(
+                str(user.Key.Hash),
+                make_geo_format(user)
+            )
+            print(result)
+        except Exception as e:
+            print(e)
+            raise e
 
 # Get a reference to our cluster
 # NOTE: For TLS/SSL connection use 'couchbases://<your-ip-address>' instead
 # cluster = Cluster('couchbase://localhost', ClusterOptions(auth))
-cluster = Cluster('couchbase://127.0.0.1', ClusterOptions(auth))
+# cluster = Cluster('couchbase://127.0.0.1', ClusterOptions(auth))
 
 # Wait until the cluster is ready for use.
-cluster.wait_until_ready(timedelta(seconds=5))
+# cluster.wait_until_ready(timedelta(seconds=5))
 
 # get a reference to our bucket
-cb = cluster.bucket("dating-data")
+# cb = cluster.bucket("dating-data")
 
-user_data_collection = cb.scope("protobufs").collection("user_data")
+# user_data_collection = cb.scope("protobufs").collection("user_data")
 
 # Get a reference to the default collection, required for older Couchbase server versions
-cb_coll_default = cb.default_collection()
-transcoder = RawBinaryTranscoder()
+# cb_coll_default = cb.default_collection()
+# transcoder = RawBinaryTranscoder()
 
 # upsert document function
 
